166647089036500078874c5e3cb640aaf1811fd8
ALEX-WANG-1000 <115406669+ALEX-WANG-1000@users.noreply.github.com>
2022-10-10 23:39:35 +0800

    Create run .py

---
+++ run .py
@@ -0,0 +1,6 @@
+win+R+cmd equals the recommand under C
+D:
+# no space between D and : 
+# when change the path from C to D, there is no need for cd exits
+D:\pythonpractice>python hello.py
+# python 调用python交互式环境
